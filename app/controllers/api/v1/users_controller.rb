26d1b8958ecc3fac3490bac66c9ed4fd2466538d
Serhii T <tocarchuk@gmail.com>
2022-04-26 12:21:36 +0200

    add user authentication token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: 201, location: [:api, user], notice: "User was successfully created!"
+      render json: user, status: 201, location: [:api, user]
     else
       render json: { errors: user.errors }, status: 422
     end
